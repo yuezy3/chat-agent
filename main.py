9d00b59e9be2f5c55c061b8390957e476fdcc61e
zeyang yue <yuezy3@163.com>
2023-04-09 08:45:32 +0000

    example req to openai

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+import os
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant",
+            "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
+
+class Chatreq(BaseModel):
+    id: str
+    msg: str
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/api/chat")
+async def make_chat(chatreq: Chatreq):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant",
+             "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )
+    return {id:chatreq["id"], msg:response['choices'][0]['message']['content']}
